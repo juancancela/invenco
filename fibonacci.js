4a2a86017de008e48b240fa629ef5c82cb8df4e7
Juan Cancela <cancela.juancarlos@gmail.com>
2017-02-24 18:47:19 -0300

    improvement: removed var for let

--- fibonacci.js
+++ fibonacci.js
@@ -4,10 +4,10 @@
  * Generator function that provides an infinite sequence of numbers of the fibonacci series
  */
 function* fibonacci() {
-    var leftValue = 0;
-    var rigthValue = 1;
+    let leftValue = 0;
+    let rigthValue = 1;
     while (true) {
-        var currentValue = leftValue;
+        let currentValue = leftValue;
         yield currentValue;
         leftValue = rigthValue;
         rigthValue = currentValue + leftValue;
